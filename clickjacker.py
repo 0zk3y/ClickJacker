6942b93d0b62bb6d107280a8822cff210321fb1c
Raziq Kadri <90907344+0zk3y@users.noreply.github.com>
2022-12-22 14:54:14 +0530

    Added urllib3

--- clickjacker.py
+++ clickjacker.py
@@ -1,15 +1,13 @@
 import requests
 
 filename = input("Enter the name of the file you want to read: ")
-try:
-    with open(filename, 'r') as f:
-        contents = f.readlines()
-    for i in contents:
-        print(i)
-        response = requests.get(url)
+with open(filename, 'r') as f:
+    contents = f.readlines()
+for i in contents:
+    print(i)
+    if not i.startswith("http://"):
+        response = requests.get(f"https://{i.replace('%0a','')}")
         if "X-Frame-Options" in response.headers:
             print("X-Frame-Options header is set")
         else:
             print("X-Frame-Options header is not set and Website might be vulnerable")
-except FileNotFoundError:
-    print("The file you entered does not exist.")
