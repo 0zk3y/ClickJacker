1b0efa9964ef7af1f3b5cb76d648f72ba2405764
Raziq Kadri <90907344+0zk3y@users.noreply.github.com>
2022-12-22 14:56:16 +0530

    Removed Warnings from urllib3

--- clickjacker.py
+++ clickjacker.py
@@ -1,12 +1,18 @@
 import requests
+import urllib3
+urllib3.disable_warnings()
 
-filename = input("Enter the name of the file you want to read: ")
+filename = input("Enter the name of the file where you have list of Domains: ")
 with open(filename, 'r') as f:
-    contents = f.readlines()
+    contents = f.read()
+    print("Following Domains will be tested for ClickJacking:")
+    print(contents)
+contents = contents.split("\n")[0:-1]
 for i in contents:
     print(i)
+    i.rstrip()
     if not i.startswith("http://"):
-        response = requests.get(f"https://{i.replace('%0a','')}")
+        response = requests.get(f"https://{i}", verify=False)
         if "X-Frame-Options" in response.headers:
             print("X-Frame-Options header is set")
         else:
