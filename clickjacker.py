9df243c624cf97367eaff046b37e6a60f078249d
Raziq Kadri <90907344+0zk3y@users.noreply.github.com>
2022-12-22 13:33:24 +0530

    Updated code to dynamically take Files

--- clickjacker.py
+++ clickjacker.py
@@ -1,8 +1,13 @@
 import requests
 
-# Send a request to the URL
-response = requests.get("http://example.com")
-
+filename = input("Enter the name of the file you want to read: ")
+try:
+    with open(filename, 'r') as f:
+        contents = f.read()
+    print(contents)
+except FileNotFoundError:
+    print("The file you entered does not exist.")
+response = requests.get()
 # Check if the X-Frame-Options header is set
 if "X-Frame-Options" in response.headers:
     print("X-Frame-Options header is set")
