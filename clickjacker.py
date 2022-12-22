820c30474efae9fb74bf55d9cf467be2aeb1c5ab
Raziq Kadri <90907344+0zk3y@users.noreply.github.com>
2022-12-22 13:40:25 +0530

    Dynamically read URL

--- clickjacker.py
+++ clickjacker.py
@@ -3,13 +3,13 @@ import requests
 filename = input("Enter the name of the file you want to read: ")
 try:
     with open(filename, 'r') as f:
-        contents = f.read()
-    print(contents)
+        contents = f.readlines()
+    for i in contents:
+        print(i)
+        response = requests.get(url)
+        if "X-Frame-Options" in response.headers:
+            print("X-Frame-Options header is set")
+        else:
+            print("X-Frame-Options header is not set and Website might be vulnerable")
 except FileNotFoundError:
     print("The file you entered does not exist.")
-response = requests.get()
-# Check if the X-Frame-Options header is set
-if "X-Frame-Options" in response.headers:
-    print("X-Frame-Options header is set")
-else:
-    print("X-Frame-Options header is not set")
