88cdd861de687342a71bceded7b6f52093deb5dc
Raziq Kadri <90907344+0zk3y@users.noreply.github.com>
2023-04-10 21:33:27 +0530

    Updated Script

--- clickjacker.py
+++ clickjacker.py
@@ -3,42 +3,40 @@ import requests
 import urllib3
 urllib3.disable_warnings()
 
+ 
+
 try:
-    filename = input("Enter the name of the file you want to read: ")
-    with open(filename, 'r') as f:
-        contents = f.read()
-        print(contents)
+    filename = input("Enter the name of the file you want to read: ")
+    if not filename:
+        print("File name cannot be empty")
+        exit()
+    with open(filename, 'r') as f:
+        contents = f.read()
+        if not contents:
+            print("File is empty")
+            exit()
+        contents = contents.split("\n")
+        for i in contents:
+            i = i.strip()
+            if not i:
+                print("Hostname cannot be empty")
+                continue
+            try:
+                if not (i.startswith("http://") or i.startswith("https://")):
+                    i = f"http://{i}"
+                response = requests.get(i, verify=False, timeout=5)
+                if "X-Frame-Options" in response.headers:
+                    print(f"{i}: X-Frame-Options header is set")
+                else:
+                    print(f"{i}: X-Frame-Options header is not set and website might be vulnerable")
+            except requests.exceptions.RequestException as e:
+                print(f"Error connecting to {i}: {e}")
+            except requests.exceptions.Timeout:
+                print(f"Connection timed out for {i}")
 except FileNotFoundError as e:
-    print(f"File not found: {e}")
-    exit()
-try:
-    contents = contents.split("\n")[0:-1]
-    for i in contents:
-        print(i)
-        i.rstrip()
-        if i == "":
-            print("Hostname cannot be empty")
-            continue
-    try:
-        if not (i.startswith("http://") or i.startswith("https://")):
-            response = requests.get(f"http://{i}", verify=False)
-        else:
-            response = requests.get(i, verify=False)
-        if "X-Frame-Options" in response.headers:
-            print("X-Frame-Options header is set")
-        else:
-            print("X-Frame-Options header is not set and Website might be Vulnerable")
-        if "X-Frame-Options" in response.headers:
-            print("X-Frame-Options header is set")
-        else:
-            print("X-Frame-Options header is not set and Website might be vulnerable")
-    except requests.exceptions.RequestException as e:
-        pass
-    except requests.exceptions.NewConnectionError as e:
-        print(f"Error connecting to {i}: {e}")
+    print(f"File not found: {e}")
 except ValueError as e:
-    print(f"Error in file contents: {e}")
-    exit()        
+    print(f"Error in file contents: {e}")
 
 print("====================================================================================================================")
 print("In case of Any issues please create an issue over Github or contact me on Twitter @0zk3y")
